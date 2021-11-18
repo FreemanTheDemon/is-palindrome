1253924f39082c92b828b63c760700b87f20f748
Freeman Bird <freemanbird@gmail.com>
2021-11-18 09:36:28 -0700

    Completed morning assignment

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,30 @@
 // Write your code below
+
+const isPalindrome = (str) => {
+    let isPalindrome = true;
+    for (let i = 0; i < str.length; i++) {
+        startChar = str[i];
+        console.log(startChar, str[str.length - (i + 1)]);
+        if (startChar !== str[str.length - (i + 1)]) {
+            isPalindrome = false;
+        }
+    }
+    return isPalindrome;
+}
+
+
+
+console.log(isPalindrome("a"));
+// returns true
+
+console.log(isPalindrome("noon"));
+// returns true
+
+console.log(isPalindrome("hello"));
+// returns false
+
+console.log(isPalindrome("Racecar"));
+// returns false
+
+console.log(isPalindrome("racecar"));
+// returns true
